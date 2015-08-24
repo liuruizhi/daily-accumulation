882cdb9369e87e14b78f6704852b8677c8f1ad24
liuruizhi <sunwei.good@163.com>
2015-08-24 16:21:47 +0800

    mybatis test

---
+++ mybatis-test/src/test/java/com/mybatis/test/UserTest.java
@@ -0,0 +1,53 @@
+package com.mybatis.test;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.mybatis.test.domain.User;
+
+public class UserTest {
+	private static Reader reader;
+	private static SqlSessionFactory sqlSessionFactory;
+	private SqlSession sqlSession;
+	
+	@Before
+	public void init() throws IOException{
+		reader = Resources.getResourceAsReader("mybatisConfiguration.xml");
+		sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+	}
+	
+	@Test
+	public void select(){
+		sqlSession = sqlSessionFactory.openSession();
+		User user = (User)sqlSession.selectOne("com.mybatis.test.UserMapper.selectUserByID", 1);
+		System.out.println(user.getName());
+	}
+	
+	@Test
+	public void selectAll(){
+		sqlSession = sqlSessionFactory.openSession();
+		List<User> list = sqlSession.selectList("com.mybatis.test.UserMapper.selectAll");
+		for(User u : list){
+			System.out.println(u.getName());
+		}
+	}
+	
+	@Test
+	public void insert(){
+		sqlSession = sqlSessionFactory.openSession();
+		User user = new User();
+		user.setName("rui");
+		sqlSession.insert("com.mybatis.test.UserMapper.insertUser", user);
+		sqlSession.commit();
+//		System.out.println(user.getName());
+	}
+}
