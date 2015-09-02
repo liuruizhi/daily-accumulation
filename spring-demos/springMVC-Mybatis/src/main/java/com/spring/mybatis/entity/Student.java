0fa01866e2ed95d0fe14e135ba02c0c9633da194
liuruizhi <sunwei.good@163.com>
2015-09-02 16:31:17 +0800

    编码修改

--- spring-demos/springMVC-Mybatis/src/main/java/com/spring/mybatis/entity/Student.java
+++ spring-demos/springMVC-Mybatis/src/main/java/com/spring/mybatis/entity/Student.java
@@ -1,11 +1,11 @@
 package com.spring.mybatis.entity;
 
 /**
- * Ñ§Éú
- * Created by lrz on 2015/9/2.
+ * å­¦ç”Ÿ
+ *  Created by lrz on 2015/9/2.
  */
 public class Student {
-	
+
 	private Integer id;
 	private String name;
 	private Integer age;
@@ -33,7 +33,7 @@ public class Student {
 	public void setAge(Integer age) {
 		this.age = age;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Student[id=" + this.id + ", name=" + this.name + ",age=" + this.age + "]";
