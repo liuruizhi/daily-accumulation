dc6edcc71a2ba4e5e6153e2c85988a7e59a9b234
liuruizhi <liuruizhi@baidu.com>
2017-11-22 12:58:50 +0800

    注释

---
+++ suan/src/com/dp/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * 动态规划
+ *
+ * @author liuruizhi
+ * @since 2017/11/22
+ */
+package com.dp;
---
+++ suan/src/com/sort/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * 排序
+ *
+ * @author liuruizhi
+ * @since 2017/11/22
+ */
+package com.sort;
