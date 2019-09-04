6f75c58dbf6c87b385e3f2c2ba76bbd3e991ced4
liuruizhi_dxm <liuruizhi@duxiaoman.com>
2019-09-04 19:33:01 +0800

    spring cloud

---
+++ spring-cloud/src/main/java/com/cloud/center/EurekaserverApplication.java
@@ -0,0 +1,18 @@
+package com.cloud.center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author liuruizhi
+ * @since 2019/9/4
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaserverApplication.class, args);
+    }
+}
