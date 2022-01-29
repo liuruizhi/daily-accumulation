b9fd3f74df6086b53cbd2303d56a02eecaca1481
liuruizhi <sunwei.good@163.com>
2022-01-29 17:13:22 +0800

    Update HeapSortFinal.java

--- suan/src/main/java/com/sort/HeapSortFinal.java
+++ suan/src/main/java/com/sort/HeapSortFinal.java
@@ -84,24 +84,24 @@ public class HeapSortFinal {
     public static void minHeap(int[] array, int heapSieze, int index) {
         int left = index * 2 + 1; // 左子节点
         int right = index * 2 + 2; // 右子节点
-        int maxValue = index; // 暂时定在Index的位置就是最小值
+        int minValue = index; // 暂时定在Index的位置就是最小值
 
         // 如果左子节点的值，比当前最小的值小，就把最小值的位置换成左子节点的位置
-        if (left < heapSieze && array[left] < array[maxValue]) {
-            maxValue = left;
+        if (left < heapSieze && array[left] < array[minValue]) {
+            minValue = left;
         }
 
         //  如果右子节点的值，比当前最小的值小，就把最小值的位置换成左子节点的位置
-        if (right < heapSieze && array[right] < array[maxValue]) {
-            maxValue = right;
+        if (right < heapSieze && array[right] < array[minValue]) {
+            minValue = right;
         }
 
         // 如果不相等，说明啊，这个子节点的值有比自己小的，位置发生了交换了位置
-        if (maxValue != index) {
-            swap(array, index, maxValue); // 就要交换位置元素
+        if (minValue != index) {
+            swap(array, index, minValue); // 就要交换位置元素
 
             // 交换完位置后还需要判断子节点是否打破了最小堆的性质。最小性质：两个子节点都比父节点大。
-            minHeap(array, heapSieze, maxValue);
+            minHeap(array, heapSieze, minValue);
         }
     }
 
